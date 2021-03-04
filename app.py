7bb9fa2a4537ff5bc848ecc07446962112b707b7
Michael Reyes <michaelreyes@jerrys-mbp-2.lan>
2021-03-03 16:22:09 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from flask_session import Session
+from geojson import Point
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+import json
+
+app=Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/linodeStreetTrees"
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    street_trees_points_query = dumps(mongo.db.linodeStreetTrees.find({}, {'_id': False}))
+    street_trees_points = json.loads(street_trees_points_query)
+    return render_template('base.html', street_trees_points=street_trees_points)
+
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
